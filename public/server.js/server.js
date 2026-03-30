e06613f2c5ee5c3bc7472444592360bde9c71168
Zillow-Zip <bn5t9cz2g5@privaterelay.appleid.com>
2026-03-30 04:05:01 -0700

    Create server.js

---
+++ public/server.js/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Correct absolute path to public folder
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+// Serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
